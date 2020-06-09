dea1bc540f5b05dc9fda675e8504c6aad0da8b0a
Omer Khan <okhanlodhi@gmail.com>
2020-06-09 13:46:32 -0500

    completed ICP1

---
+++ Source/ICP_1/Question3.py
@@ -0,0 +1,8 @@
+# input any STR string from the console
+string = str(input("Input any string: "))
+
+# convert the string to all lower case
+string.lower()
+
+# print string while making correct replacements
+print(string.replace("python", "pythons"))
